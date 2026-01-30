e133108e64c8b90fe47a9b2b7fcdc42995ec5cb9
thanishar25am-png <thanisha.r_25am@kgkite.ac.in>
2026-01-30 11:32:30 +0530

    problem_04 solved

--- problem_04.py
+++ problem_04.py
@@ -2,8 +2,8 @@
 # Find and fix the error
 
 numbers = [45, 12, 78, 34, 89, 23]
-largest = numbers[0]
+largest = numbers[4]
 for i in range(len(numbers)):
     if numbers[i] > largest:
         largest = numbers[i]
-print(f"Largest number is: {largest}")
+print("Largest number is:" , largest)
